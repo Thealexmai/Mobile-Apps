5d4153a486ef4d1a160271a282ed73554a84f88b
Alex <amai2@u.rochester.edu>
2016-11-25 10:34:07 -0500

    Create Login for Persistent Storage

---
+++ Project2_Alex_Mai/Project1_Alex_Mai/Login.swift
@@ -0,0 +1,9 @@
+//
+//  Login.swift
+//  Project2_Alex_Mai
+//
+//  Created by Alex Mai on 11/25/16.
+//  Copyright © 2016 Mobile Application Development. All rights reserved.
+//
+
+import Foundation
