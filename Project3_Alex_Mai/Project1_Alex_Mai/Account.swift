4b085052e5efe6c1ae251a82345ef0f34073dcb2
Alex <amai2@u.rochester.edu>
2016-12-08 09:34:33 -0500

    Remove User class
    Easier

--- Project3_Alex_Mai/Project1_Alex_Mai/Account.swift
+++ Project3_Alex_Mai/Project1_Alex_Mai/Account.swift
@@ -9,18 +9,25 @@
 import Foundation
 
 //This class allows the user to create an account
-class Account:User {
+class Account: NSObject {
+    var name: String
+    var gender: String
+    var age: Int
+    var image: String
     var login:String
     var password: String
     var email: String
     var emergencyContact: String
     
     init(_ name: String, _ gender: String, _ age: Int, _ image: String, _ login:String, _ password: String, _ email:String, _ emergencyContact:String) {
+        self.name = name
+        self.gender = gender
+        self.age = age
+        self.image = image
         self.login = login
         self.password = password
         self.email = email
         self.emergencyContact = emergencyContact
-        super.init(name, gender, age, image)
     }
     
     
